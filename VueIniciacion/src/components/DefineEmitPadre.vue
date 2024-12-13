84a8680b505f54a93389ff4164a253e9f5190bfc
mcalex468 <alexmaco468@gmail.com>
2024-12-13 16:39:43 +0100

    c

--- VueIniciacion/src/components/DefineEmitPadre.vue
+++ VueIniciacion/src/components/DefineEmitPadre.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-      <h1>Nombre recibido: {{ userName }}</h1>
-      <!-- Escuchamos el evento submitName del hijo -->
-      <DefineEmitHijo @submitName="handleName" />
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import ChildComponent from './ChildComponent.vue';
+  <div>
+    <h1>Nombre recibido: {{ userName }}</h1>
+    <!-- Escuchamos el evento submitName del hijo -->
+    <DefineEmitHijo @submitName="handleName" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
 import DefineEmitHijo from './DefineEmitHijo.vue';
-  
-  // Variable reactiva para almacenar el nombre recibido
-  const userName = ref('');
-  
-  // Función que actualiza el nombre
-  const handleName = (name) => {
-    userName.value = name;
-  };
-  </script>
-  
+
+// Variable reactiva para almacenar el nombre recibido
+const userName = ref('');
+
+// Función que actualiza el nombre
+const handleName = (name) => {
+  userName.value = name;
+};
+</script>
