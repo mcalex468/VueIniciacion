09f1979caee143a27ad9b116a7289fb7f45744b1
mcalex468 <alexmaco468@gmail.com>
2024-12-13 16:36:28 +0100

    c

---
+++ VueIniciacion/src/components/DefineEmitHijo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <form @submit.prevent="submitName">
+        <input type="text" v-model="name" placeholder="Escribe tu nombre" />
+        <button type="submit">Enviar Nombre</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  // Declaramos el evento que el hijo emitirá
+  const emit = defineEmits(['submitName']);
+  
+  // Variable reactiva para el nombre
+  const name = ref('');
+  
+  // Función que emite el nombre al padre
+  const submitName = () => {
+    emit('submitName', name.value);
+  };
+  </script>
+  
---
+++ VueIniciacion/src/components/DefineEmitPadre.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Nombre recibido: {{ userName }}</h1>
+      <!-- Escuchamos el evento submitName del hijo -->
+      <DefineEmitHijo @submitName="handleName" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import ChildComponent from './ChildComponent.vue';
+import DefineEmitHijo from './DefineEmitHijo.vue';
+  
+  // Variable reactiva para almacenar el nombre recibido
+  const userName = ref('');
+  
+  // Función que actualiza el nombre
+  const handleName = (name) => {
+    userName.value = name;
+  };
+  </script>
+  
