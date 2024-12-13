657f58cea3aba8e558fd965ab0b72de73848d613
mcalex468 <alexmaco468@gmail.com>
2024-12-13 17:21:54 +0100

    c

--- VueIniciacion/src/components/DefineEmitPadre.vue
+++ VueIniciacion/src/components/DefineEmitPadre.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-<<<<<<< HEAD
     <h1>Nombre recibido: {{ userName }}</h1>
     <!-- Escuchamos el evento submitName del hijo -->
     <DefineEmitHijo @submitName="handleName" />
-=======
-    <!-- El padre escucha el evento 'enviarM' del hijo y lo maneja con la función handleMessage -->
-    <DefineEmitHijo @enviarM="nombre = $event" />
-    <h1>{{ nombre }}</h1>
->>>>>>> 81b8776 (c)
   </div>
 </template>
 
@@ -16,7 +10,6 @@
 import { ref } from 'vue';
 import DefineEmitHijo from './DefineEmitHijo.vue';
 
-<<<<<<< HEAD
 // Variable reactiva para almacenar el nombre recibido
 const userName = ref('');
 
@@ -25,7 +18,3 @@ const handleName = (name) => {
   userName.value = name;
 };
 </script>
-=======
-const nombre = ref('');
-</script>
->>>>>>> 81b8776 (c)
