45dfa08c51bde5b8b512ced7b39c7cc76223ef5e
mcalex468 <alexmaco468@gmail.com>
2024-12-17 10:07:18 +0100

    c

--- Router/src/components/Products.vue
+++ Router/src/components/Products.vue
@@ -3,11 +3,12 @@
     <div v-for="producto in productos" :key="producto.id">
         {{ producto.name }} - {{ producto.description }}
         <!-- En el routerLink pasamos todo lo que hayamos pasado por el path del index.js-->
-        <RouterLink :to="'/productDetail/' + productos.id + '/' + productos.name + 
-        '/' + productos.description">{{ producto.id }}</RouterLink>
+        <RouterLink :to="'/productDetail/' + producto.id + '/' + producto.name + 
+        '/' + producto.description">{{ producto.id }}</RouterLink>
     </div>
 </template>
 
 <script setup>
 import productos from '@/assets/productos.json';
+import { RouterLink,RouterView } from 'vue-router';
 </script>
--- Router/src/views/ProductDetailView.vue
+++ Router/src/views/ProductDetailView.vue
@@ -1,7 +1,11 @@
 <template>
-    
+    <h2>Detail Product</h2>
+    <p>{{ $route.params.id }}</p>
+    <p>{{ $route.params.name }}</p>
+    <p>{{ $route.params.description }}</p>
 </template>
 
 <script setup>
 import productos from '@/assets/productos.json';
+import { RouterLink } from 'vue-router';
 </script>
